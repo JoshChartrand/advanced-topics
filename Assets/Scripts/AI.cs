2007f65ba3e97b31d4623b3be225cc647d68c330
Airi Chan <jasminechan@cmail.carleton.ca>
2018-03-22 21:44:51 -0400

    Started adding environment, worked on fox mount code

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AI : MonoBehaviour {
+
+	public GameObject catMesh;
+	public float moveForce = 0f;
+	private Rigidbody rbody;
+	public Vector3 moveDir;
+	public LayerMask whatIsWall;
+	public float maxDistFromWall = 0f;
+
+	public int DirMinTime = 5;
+	public int DirMaxTime = 20;
+	private float Dirtimez = 0;
+	private float DirRandTime;
+
+	public int StaMinTime = 10;
+	public int StaMaxTime = 20;
+	private float Statimez = 0;
+	private float StaRandTime;
+
+	public int WaiMinTime = 1;
+	public int WaiMaxTime = 8;
+	private float Waitimez = 0;
+	private float WaiRandTime;
+
+	//public Texture2D catTex;
+	//public Texture2D zombieTex;
+	//private SkinnedMeshRenderer rend;
+
+	//private bool zombie;
+
+	//public Text score; 
+	//private int num;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody> ();
+		moveDir = ChooseDirection ();
+		transform.rotation = Quaternion.LookRotation (moveDir);
+		//rend = catMesh.GetComponent<SkinnedMeshRenderer> ();
+		//rend.material.mainTexture = catTex;
+		//zombie = false;
+		//GlobalValues.ZombieCats = 0; 
+		DirRandTime = Random.Range (DirMinTime, DirMaxTime);
+		StaRandTime = Random.Range (StaMinTime, StaMaxTime);
+		WaiRandTime = Random.Range (WaiMinTime, WaiMaxTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Statimez + StaRandTime > Time.time){
+			rbody.velocity = moveDir * moveForce;
+			if(Physics.Raycast(transform.position, transform.forward, maxDistFromWall, whatIsWall)){
+				moveDir = ChooseDirection ();
+				transform.rotation = Quaternion.LookRotation (moveDir);
+			}
+
+			if (Dirtimez + DirRandTime < Time.time){
+				moveDir = ChooseDirection ();
+				transform.rotation = Quaternion.LookRotation (moveDir);
+				Dirtimez = Time.time;
+				DirRandTime = Random.Range (DirMinTime, DirMaxTime);
+			}
+		}
+		else{
+			//print ("pause");
+			rbody.velocity = new Vector3(0,0,0);
+			if(Waitimez + WaiRandTime < Time.time){
+				//print ("unpause");
+				Statimez = Time.time;
+				StaRandTime = Random.Range (StaMinTime, DirMaxTime);
+				Waitimez = Time.time;
+				WaiRandTime = Random.Range (WaiMinTime, WaiMaxTime);
+			}
+		}
+	}
+
+	Vector3 ChooseDirection()
+	{
+		System.Random ran = new System.Random ();
+		int i = ran.Next (0,3);
+		Vector3 temp = new Vector3 ();
+
+		if(i==0){
+			temp = transform.forward;
+		}
+		else if(i == 1){
+			temp = transform.right;
+		}
+		else if(i == 2){
+			temp = transform.right;
+		}
+		return temp;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		
+	}
+}
---
+++ Assets/Scripts/MountHorse.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MountHorse : MonoBehaviour {
+
+	bool IsOverlapping = false;
+
+	GameObject Player = null;
+
+	GameObject Mountable = null;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (IsOverlapping && (OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger) > 0 || OVRInput.Get (OVRInput.Axis1D.SecondaryIndexTrigger) > 0) && Player.transform.parent != Mountable) {
+			Debug.Log ("Mounting");
+			Player.transform.SetParent(Mountable.transform);
+			Collider[] Colliders = Player.GetComponentsInChildren<Collider> ();
+			foreach (Collider col in Colliders) {
+				Physics.IgnoreCollision (col, Mountable.GetComponent<Collider> ());
+			}
+			Player.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+			//Player.GetComponent<OVRPlayerController>()
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Saddle") {
+			IsOverlapping = true;
+			Mountable = other.transform.parent.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Saddle") {
+			IsOverlapping = false;
+			//Mountable = null;
+		}
+	}
+
+}
