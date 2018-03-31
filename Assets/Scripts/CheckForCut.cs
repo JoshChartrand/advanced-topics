f49ad72e255425a6b8a5da0189ded592c490fdd7
Airi Chan <jasminechan@cmail.carleton.ca>
2018-03-31 16:14:20 -0400

    Added a cut script, added animation state for fox running and fixed its colliders

---
+++ Assets/Scripts/CheckForCut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForCut : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col) {
+		Debug.Log (col.relativeVelocity.magnitude);
+		//col.gameObject.tag == "Weapon" && 
+		if (col.relativeVelocity.magnitude >= 20) {
+			GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
+		}
+	}
+}
